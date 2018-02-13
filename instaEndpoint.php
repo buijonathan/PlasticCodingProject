057e0e9808e721565baccd6308be19dac6e16310
buijonathan <jonathandbui@gmail.com>
2018-02-13 09:13:55 -0800

    testing serverside key gen

--- instaEndpoint.php
+++ instaEndpoint.php
@@ -1,17 +1,23 @@
 
-<script>
-  var arr = window.location.href.split("#");
-  if(arr.length == 2) {
-	    window.location.href = "http://dev-env.imxpud8g9s.us-west-2.elasticbeanstalk.com/instaEndpoint.php?" + arr[1]; 
-  } else {
-	  document.write("could not find #");
-  }
-
-</script>
 
 <?php
+$code = $_GET["code"];
+$url = 'https://api.instagram.com/oauth/access_token';
+$data = array('client_secret' => '91ece73e7b864be6b3144f42a73b5ba2 ', 'client_id' => '649a75e6c43448b9b9927cee68fcf55b', 'grant_type' => 'authorization_code', 'redirect_uri' => '#', 'code' => $code);
 
-var_dump($_GET["access_token"]);
+// use key 'http' even if you send the request to https://...
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data)
+    )
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+if ($result === FALSE) { /* Handle error */ }
+
+var_dump($result);
 
 
 ?>
