0e3a7feab608770ec7607bcb3d14615490b39bdb
buijonathan <jonathandbui@gmail.com>
2018-02-14 08:56:46 -0800

    ''

--- callback.php
+++ callback.php
@@ -1,20 +1,7 @@
 <?php
 
-$url = '    https://api.instagram.com/v1/subscriptions/';
-$data = array('client_id' => '649a75e6c43448b9b9927cee68fcf55b', 'client_secret' => '91ece73e7b864be6b3144f42a73b5ba2', 'object' => 'user', 'aspect' => 'media', 'verify_token' => 'aToken', 'redirect_uri' => 'http://dev-env.imxpud8g9s.us-west-2.elasticbeanstalk.com/callback.php');
-
-// use key 'http' even if you send the request to https://...
-$options = array(
-    'http' => array(
-        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        'method'  => 'POST',
-        'content' => http_build_query($data)
-    )
-);
-$context  = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
-if ($result === FALSE) { /* Handle error */ }
-
-var_dump($result);
+if($_GET['hub.mode'] == "subscribe") {
+	echo $_GET['hub.challenge'];
+}
 
 ?>
