03fb4cffa60c7862f804f9f73e624b3e3ea98d48
buijonathan <jonathandbui@gmail.com>
2018-02-14 10:11:05 -0800

    ''

--- callback.php
+++ callback.php
@@ -1,6 +1,16 @@
 <?php
 if($_GET['hub_mode'] == "subscribe") {
 	echo $_GET['hub_challenge'];
+} else {
+	$file = fopen("testData", 'a');
+	$getData = "";
+	$putData = "";
+	var_export($getData, true);
+	var_export($putData, true);
+	fwrite($getData . " + " . $putData);
 }
+
+
+
 
 ?>
