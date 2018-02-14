f6f6af23799061808e71d3c86a9e47c3de4d5a1f
buijonathan <jonathandbui@gmail.com>
2018-02-14 08:41:18 -0800

    ''

--- instaEndpoint.php
+++ instaEndpoint.php
@@ -2,8 +2,9 @@
 
 <?php
 $code = $_GET["code"];
+var_dump($_GET);
 $url = 'https://api.instagram.com/oauth/access_token';
-$data = array('client_secret' => '91ece73e7b864be6b3144f42a73b5ba2 ', 'client_id' => '649a75e6c43448b9b9927cee68fcf55b', 'grant_type' => 'authorization_code', 'redirect_uri' => 'http://dev-env.imxpud8g9s.us-west-2.elasticbeanstalk.com/instaEndpoint.php', 'code' => $code);
+$data = array('client_id' => '649a75e6c43448b9b9927cee68fcf55b', 'client_secret' => '91ece73e7b864be6b3144f42a73b5ba2', 'grant_type' => 'authorization_code', 'redirect_uri' => 'http://dev-env.imxpud8g9s.us-west-2.elasticbeanstalk.com/instaEndpoint.php', 'code' => $code);
 
 // use key 'http' even if you send the request to https://...
 $options = array(
