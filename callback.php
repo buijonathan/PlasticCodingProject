308c8efe7dccf7eef9fe7f3ebeb65a05087ed686
buijonathan <jonathandbui@gmail.com>
2018-02-14 10:32:36 -0800

    ''

--- callback.php
+++ callback.php
@@ -2,12 +2,14 @@
 if($_GET['hub_mode'] == "subscribe") {
 	echo $_GET['hub_challenge'];
 } else {
+	
 	$fileName = "textData.txt";
 	$file = fopen($fileName, 'a') or die('Cannot open file:  ' . $fileName);
 
 	var_export($getData, true);
 	var_export($putData, true);
 	fwrite("writing: " . $getData . " + " . $putData);
+	echo ("writing: " . $getData . " + " . $putData);
 	fclose($file);
 
 	
--- readTextFile.php
+++ readTextFile.php
@@ -3,6 +3,6 @@
 $fileName = "textData.txt";
 $file = fopen($fileName, 'a') or die('Cannot open file:  ' . $fileName);
 $data = fread($file, filesize($fileName));
-echo $data;
+echo "reading: " . $data;
 
 ?>
