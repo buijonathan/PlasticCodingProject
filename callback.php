4c9152667ab6e00ba23ae87c1529d1799d00b7cc
buijonathan <jonathandbui@gmail.com>
2018-02-14 22:42:45 -0800

    push commit

--- callback.php
+++ callback.php
@@ -7,9 +7,10 @@ echo "trying to connect";
 $servername = "aa1a0jahjffztnz.cvvm8c9essu3.us-west-2.rds.amazonaws.com:3306";
 $username = "admin";
 $password = "password";
+$dbName = "users";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbName);
 
 // Check connection
 if ($conn->connect_error) {
@@ -23,7 +24,17 @@ if ($conn->query($sql) === TRUE) {
 } else {
     echo "Error creating database: " . $conn->error;
 }
-echo "created data base";
+echo "created data base";	
+
+$sql = "CREATE TABLE UserIds (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
+)";
+
+
 $conn->close();
 
 	
