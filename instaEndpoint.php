a9ea3682948faae7b56fae7818b6ffeded9cb3d0
buijonathan <jonathandbui@gmail.com>
2018-02-27 08:36:53 -0800

    ''

--- instaEndpoint.php
+++ instaEndpoint.php
@@ -23,31 +23,49 @@ echo("<br><br>");
 $json = json_decode($result, true);
 echo $json['access_token'];
 
-$client = new http\Client;
-$request = new http\Client\Request;
-
-$body = new http\Message\Body;
-$body->addForm(array(
-  'client_id' => '649a75e6c43448b9b9927cee68fcf55b',
-  'client_secret' => '91ece73e7b864be6b3144f42a73b5ba2',
-  'object' => 'user',
-  'aspect' => 'media',
-  'verify_token' => $json['access_token'];
-  'callback_url' => 'http://dev-env.imxpud8g9s.us-west-2.elasticbeanstalk.com/callback.php'
-), NULL);
-
-$request->setRequestUrl('https://api.instagram.com/v1/subscriptions/');
-$request->setRequestMethod('POST');
-$request->setBody($body);
+$request = new HttpRequest();
+$request->setUrl('https://api.instagram.com/v1/subscriptions/');
+$request->setMethod(HTTP_METH_POST);
 
 $request->setHeaders(array(
+  'Postman-Token' => '60cdc0f8-9312-01da-1e2b-78a81e1e7674',
   'Cache-Control' => 'no-cache',
-  'Content-Type' => 'application/x-www-form-urlencoded'
+  'Content-Type' => 'application/x-www-form-urlencoded',
+  'content-type' => 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
 ));
 
-$client->enqueue($request)->send();
-$response = $client->getResponse();
-echo ("<br><br>");
-echo $response->getBody();
+$request->setBody('------WebKitFormBoundary7MA4YWxkTrZu0gW
+Content-Disposition: form-data; name="client_id"
+
+649a75e6c43448b9b9927cee68fcf55b
+------WebKitFormBoundary7MA4YWxkTrZu0gW
+Content-Disposition: form-data; name="client_secret"
+
+91ece73e7b864be6b3144f42a73b5ba2
+------WebKitFormBoundary7MA4YWxkTrZu0gW
+Content-Disposition: form-data; name="object"
+
+user
+------WebKitFormBoundary7MA4YWxkTrZu0gW
+Content-Disposition: form-data; name="aspect"
+
+media
+------WebKitFormBoundary7MA4YWxkTrZu0gW
+Content-Disposition: form-data; name="verify_token"
+
+' . $json['access_token'] . '
+------WebKitFormBoundary7MA4YWxkTrZu0gW
+Content-Disposition: form-data; name="callback_url"
+
+http://dev-env.imxpud8g9s.us-west-2.elasticbeanstalk.com/callback.php
+------WebKitFormBoundary7MA4YWxkTrZu0gW--');
+
+try {
+  $response = $request->send();
+
+  echo $response->getBody();
+} catch (HttpException $ex) {
+  echo $ex;
+}
 
 ?>
