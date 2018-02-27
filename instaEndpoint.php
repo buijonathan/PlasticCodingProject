f3c54c3f85bb1b981cbfa802904f9b2daea20ecc
buijonathan <jonathandbui@gmail.com>
2018-02-27 14:36:16 -0800

    ''

--- instaEndpoint.php
+++ instaEndpoint.php
@@ -42,22 +42,21 @@ $sql = "SELECT * FROM users WHERE userId=" . $json['user']['id'];
 echo ($sql . "<br>");
 
 $result = $conn->query($sql);
-var_dump($result);
-//if ($result->num_rows > 0) {
- //   echo "user already exists!<br>"
-    //while($row = $result->fetch_assoc()) {
-    //    var_dump($row);
-	//	echo "<br>";
-    //}
-//} else {
-//	echo "User not found, adding";
-    //$sql = "INSERT INTO users (userId)
-//VALUES (\' " . $json['user']['id'] . "\')";
+if ($result->num_rows > 0) {
+    echo "user already exists!<br>"
+    while($row = $result->fetch_assoc()) {
+        var_dump($row);
+		echo "<br>";
+    }
+} else {
+	echo "User not found, adding";
+    $sql = "INSERT INTO users (userId)
+VALUES (\' " . $json['user']['id'] . "\')";
 
-	//if ($conn->query($sql) === TRUE) {
-	//	echo "New record created successfully";
-	//} else {
-	//	echo "Error: " . $sql . "<br>" . $conn->error;
-	//}
-//}
+	if ($conn->query($sql) === TRUE) {
+		echo "New record created successfully";
+	} else {
+		echo ("Error: " . $sql . "<br>" . $conn->error);
+	}
+}
 ?>
