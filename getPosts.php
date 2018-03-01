c1910f4c81569b9f625952a68048c8248d57b9e2
buijonathan <jonathandbui@gmail.com>
2018-03-01 14:41:45 -0800

    ''

--- getPosts.php
+++ getPosts.php
@@ -16,18 +16,17 @@ echo "Connected successfully <br>";
 $sql = "SELECT * FROM posts";
 $postResults = $conn->query($sql);
 echo $conn->error;
-$counter = 0;
-$data = array();
-while ($row = $postResults->fetch_array(MYSQLI_ASSOC)) {
+//$data = array();
+//while ($row = $postResults->fetch_array(MYSQLI_ASSOC)) {
 	
-	array_push($data, json_encode($row));
+//	array_push($data, json_encode($row));
 	
-}
+//}
 
-$json = array();
-$json['size'] = sizeof($data);
-$json['data'] = $data;
-echo json_decode($json, true);
+//$json = array();
+//$json['size'] = sizeof($data);
+//$json['data'] = $data;
+//echo json_decode($json, true);
 
 
 >
