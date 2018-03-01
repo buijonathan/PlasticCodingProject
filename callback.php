95cd4a74c741debc5721a6e740de7eaa272ab2ad
buijonathan <jonathandbui@gmail.com>
2018-03-01 08:35:09 -0800

    ''

--- callback.php
+++ callback.php
@@ -100,7 +100,8 @@ while ($row = $userResults->fetch_array(MYSQLI_ASSOC)) {
 		echo("long: " . $post['location']['longitude']	. "<br>");
 		echo("name: " . $post['location']['name'] . "<br>");
 		
-		$result = $sql = "SELECT * FROM posts WHERE mediaId=" . trim($post['id']);
+		$sql = "SELECT * FROM posts WHERE mediaId=" . trim($post['id']);
+		$result = $conn->query($sql);
 		if ($result->num_rows > 0) {
 			echo "media already exists!";
 		} else {
