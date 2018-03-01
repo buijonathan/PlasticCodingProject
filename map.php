f8e4e1b63762b627ef244a2a5761d1cfaf4b4b84
buijonathan <jonathandbui@gmail.com>
2018-03-01 15:21:41 -0800

    ''

--- map.php
+++ map.php
@@ -22,7 +22,7 @@
     <div id="map"></div>
     <script>
       var map;
-	  var jsonString = "<?php include 'getPosts.php'; ?>";
+	  var count = 0;
 	  document.write(document.body + " " + jsonString);
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
@@ -30,6 +30,20 @@
           zoom: 12
         });
       }
+	  function updateMap() {
+		var jsonString = "<?php include 'getPosts.php'; ?>";
+		var jsonObj = JSON.parse(jsonString);
+		var data = jsonObj.data;
+		for(var i = 0; i < data.length; i++) {
+			var post = data[i];
+			var marker = new google.maps.Marker({
+				position: {lat: 37.303189, lng: -121.804466},
+				map: map,
+				title: post.mediaId;
+			});
+		}
+		updateMap();
+	  }
     </script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBNFq9N7BBRZxthhRQ_a9-gc3Lq7di0g68&callback=initMap"
     async defer></script>
